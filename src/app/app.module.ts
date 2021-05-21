9dd8a3f385681d575d0e292760ed41acf14a74cc
Osca-M <oscamwongera@gmail.com>
2021-05-21 11:07:28 +0300

    Create a scaleline component to handle the scaleline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
+import { ScalelineComponent } from './components/scaleline/scaleline.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    ScalelineComponent
   ],
   imports: [
     BrowserModule
