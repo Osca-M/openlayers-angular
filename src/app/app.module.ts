c32a983db2f154c422478f3de4d15e874f00d8d0
Osca-M <oscamwongera@gmail.com>
2021-05-21 11:39:00 +0300

    Add moise position component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
 import { ScalelineComponent } from './components/scaleline/scaleline.component';
+import { MousePositionComponent } from './components/mouse-position/mouse-position.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
-    ScalelineComponent
+    ScalelineComponent,
+    MousePositionComponent
   ],
   imports: [
     BrowserModule
